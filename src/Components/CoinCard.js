d3ad58bcd90b12707ce04d6c040ef6c0533a63c8
William <Billy.campbell17@gmail.com>
2021-02-09 16:48:24 -0500

    istalled watchman

---
+++ src/Components/CoinCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Stylesheet, Image} from 'react-native';
+import {images} from '../Utils/CoinIcons';
+
+const style = Stylesheet.create({
+    container: {
+       display:"Flex",
+       marginBottom: 20,
+       borderBottomColor: '#e5e5e5',
+         
+    }
+})
