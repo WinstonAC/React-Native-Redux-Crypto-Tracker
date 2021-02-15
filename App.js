a00de0aeaf2a887b6e22859cb8934b878a61057e
William <Billy.campbell17@gmail.com>
2021-02-15 17:44:39 -0500

    added folders to src utls actinoType Constants

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world</Text>
+      <Text>Dicks</Text>
       <StatusBar style="auto" />
     </View>
   );
